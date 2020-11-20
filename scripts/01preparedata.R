9f3df78550bdca62a0e5db413c4bc312e321ff73
Hannah Metzler <hannahmetzler1@gmail.com>
2020-11-20 19:30:05 +0100

    removed weekday baselines, effect of excluding authors

--- scripts/01preparedata.R
+++ scripts/01preparedata.R
@@ -1,28 +1,30 @@
 
-#packages
-library(tidyr)
 
-# Read data
-or_cs <- read.csv("data_raw/VolOregonCampaignSuicide.csv") # campaign AND suicide terms: with keywords about the breaking the silence campaign, with suicide as keyword, from oregon
-or_c  <- read.csv("data_raw/VolOregonCampaign.csv") # campaign tweets without suicide term: tweets with keywords about the campaign only,  from oregon
+# Read data: oregon data
+or_ca <- read.csv("data_raw/VolOregonCampaignWithAuthors.csv") # campaign terms narrowly defined, tweets from authors breaksilenceOR, 800723TALK & lines_for_life included
+or_c  <- read.csv("data_raw/VolOregonCampaignWithoutAuthors.csv") # campaign terms narrowly defined, tweets from authors breaksilenceOR, 800723TALK & lines_for_life excluded
+or_pa <- read.csv("data_raw/VolOregonPreventionWithAuthors.csv") #prevention tweets: lifeline number, lifeline keywords, betheone2 keywords and hashtags
+or_p <- read.csv("data_raw/VolOregonPreventionWithoutAuthors.csv") #prevention tweets: lifeline number, lifeline keywords, betheone2 keywords and hashtags
 or_tot  <- read.csv("data_raw/VolOregonTotal.csv") #total tweet volume oregon
-or_csn <- read.csv("data_raw/VolOregonCampaignNarrowSuicide.csv") #campaign AND suicide AND broader selection of hashtags around prevention
-wa_cs <- read.csv("data_raw/VolWashingtonCampaignSuicide.csv") # campaign AND suicide tweets: with keywords about the breaking the silence campaign, with suicide as keyword, from oregon
-wa_c  <- read.csv("data_raw/VolWashingtonCampaign.csv") # campaign tweets without suicide term: tweets with keywords about the campaign only,  from oregon
-wa_tot  <- read.csv("data_raw/VolWashingtonTotal.csv") #total tweet volume oregon
-wa_csn <- read.csv("data_raw/VolWashingtonCampaignNarrowSuicide.csv") #campaign AND suicide AND broader selection of hashtags around prevention
-#bts_account <- read.csv("data_raw/VolTwitterAccountBreakSilenceOR.csv") #tweets posted by the account of the campaign itself
+
+#washington data
+wa_ca <- read.csv("data_raw/VolWashingtonCampaignWithAuthors.csv") 
+wa_c  <- read.csv("data_raw/VolWashingtonCampaignWithoutAuthors.csv") 
+wa_pa <- read.csv("data_raw/VolWashingtonPreventionWithAuthors.csv") 
+wa_p <- read.csv("data_raw/VolWashingtonPreventionWithoutAuthors.csv")
+wa_tot  <- read.csv("data_raw/VolWashingtonTotal.csv") 
 
 #rename columns
 names(or_c) <- c("date","end","c_n")
-names(or_cs) <- c("date","end","cs_n") 
-names(or_csn) <- c("date","end","csn_n") 
+names(or_ca) <- c("date","end","ca_n") 
+names(or_p) <- c("date","end","prev_n") 
+names(or_pa) <- c("date","end","preva_n")
 names(or_tot) <- c("date","end","tot_n") 
 names(wa_c) <- c("date","end","c_n") 
-names(wa_cs) <- c("date","end","cs_n") 
+names(wa_ca) <- c("date","end","ca_n") 
 names(wa_tot) <- c("date","end","tot_n")  
-names(wa_csn) <- c("date","end","csn_n")
-#names(bts_account) <- c("date","end","acc_n") #delete
+names(wa_p) <- c("date","end","prev_n")
+names(wa_pa) <- c("date","end","preva_n")
 
 #function: change date format, remove last date
 prepdates <- function(df) {
@@ -30,71 +32,46 @@ prepdates <- function(df) {
     mutate(date = as.Date(date)) %>%
     select(-end) %>%  #remove the column with ending date, end is always 24h after start
     arrange(date)
-  #delete rest of function until retunr(df)
-    # #subtract tweets of the campaign account itself - delete this later
-    # df[1:nrow(df),2] = df[1:nrow(df),2]-bts_account[1:nrow(df),"acc_n"]
-    # #one tweet by the bts account did not contain any campaign keyword and is therefore not included in the campaign counts. 
-    # #To prevent the count going below 0, add 1 if this happens, to correct for this, download all volumes while excluding account terms from the beginning
-    # for(i in seq(1:nrow(df)))
-    #     {
-    #      if(df[i,2]==-1){df[i,2]=df[i,2]+1}
-    #       }
   return(df) }
 
 #function to calculate proportion, baseline and transform to long data format
-prop_bl_long <- function(df_tot, df_c, df_cs, df_csn) {
+prop_bl_long <- function(df_tot, df_c, df_ca, df_p, df_pa) {
   
   # to debug: when not using the following code as a function, but assign input directly
-  #df_tot <- or_tot; df_c <- or_c; df_cs <- or_cs; df_csn<- or_csn 
- #df_tot <- wa_tot; df_c <- wa_c; df_cs <- wa_cs; df_csn<- wa_csn 
+  # df_tot <- or_tot; df_c <- or_c; df_ca <- or_ca; df_p<- or_p; df_pa <-or_pa
+  #df_tot <- wa_tot; df_c <- wa_c; df_ca <- wa_ca; df_p<- wa_p 
   
   #apply function defined above to format date and remove last date
-  df_cs <- prepdates(df_cs)
+  df_ca <- prepdates(df_ca)
   df_c <- prepdates(df_c)
-  df_csn <- prepdates(df_csn) 
+  df_p <- prepdates(df_p) 
+  df_pa <- prepdates(df_pa) 
   df_tot <- prepdates(df_tot)
   
   #proportions with terms per category 
   df_all <- df_tot%>%
-    left_join(df_cs)%>%
-    left_join(df_csn) %>% 
     left_join(df_c)%>%
+    left_join(df_ca)%>%
+    left_join(df_p) %>% 
+    left_join(df_pa) %>% 
     mutate(c_pr = c_n/tot_n*100,
-           cs_pr = cs_n/tot_n*100,
-           csn_pr = csn_n/tot_n*100)
+           ca_pr = ca_n/tot_n*100,
+           prev_pr = prev_n/tot_n*100,
+           preva_pr = preva_n/tot_n*100) %>% 
   
   # define data period to be analysed: from start of 2018 as baseline (campaign from April 7-14 2019) until end of november 2019 (to exclude christmas)
-  df_all <- df_all %>% filter(date >="2018-01-01" & date <"2019-11-30")
+  filter(date >="2018-01-01" & date <"2019-11-30")
   
-  #baseline: the year 2018
-  baseline <- df_all %>% 
-    mutate(weekday = lubridate::wday(date, label=T, abbr=F))%>% #transform date to day of the week, first day: Tuesday
-    filter(date >= as.Date("2018-01-01") & date <= as.Date("2018-12-31")) %>% #define when baseline period ends
-    group_by(weekday) %>% 
-    #campaign and suicide keywords
-    summarize(cs_bl = median(cs_pr), 
-              #campain keywords
-              c_bl = median(c_pr),
-              #campaign terms narrowly defined, suicide
-              csn_bl = mean(csn_pr)) %>% #meadian is zero, and devision by zero will cause problems, therefore take the mean
-    #to avoid baselines of 0
-    mutate(csn_bl=ifelse(csn_bl==0, csn_bl+0.00000000001, csn_bl+0))
-  
-  #add weekday labels to df_all
-  df_all <- df_all %>% 
-    mutate(weekday = lubridate::wday(date, label=T, abbr=F))  %>% 
-    #add baseline for each weekday to df_all
-    inner_join(baseline)
   
   #produce long version of the data
   df_long <- df_all%>%
     pivot_longer(
-      -c(weekday, date, tot_n), 
+      -c(date, tot_n), 
       names_to = c("keywords", ".value"), 
       names_sep = "_" 
       #values_drop_na = TRUE
     ) %>%
-    mutate(keywords = dplyr::recode(keywords, c = "Campaign", cs="Campaign + Suicide", csn="Campaign narrow + Suicide")) %>% 
+    mutate(keywords = dplyr::recode(keywords, c = "Campaign wo authors", ca="Campaign with authors", prev ="Prevention wo authors", preva="Prevention with authors")) %>% 
     mutate(keywords = factor(keywords))%>%
     rename(keywords_n = n)
   
@@ -117,23 +94,31 @@ prop_bl_long <- function(df_tot, df_c, df_cs, df_csn) {
   return(df_long)
   
 } #end of prop_bl_long function
-# print("Processing Oregon")
-# df_oregon <- prop_bl_long(or_tot, or_c, or_cs, or_csn) %>% 
-#   mutate(state = factor("Oregon"))
+print("Processing Oregon")
+df_oregon <- prop_bl_long(or_tot, or_c, or_ca, or_p, or_pa) %>% 
+  mutate(state = factor("Oregon"))
 print("Processing Washington")
-df_washington<- prop_bl_long(wa_tot, wa_c, wa_cs, wa_csn) %>%
+df_washington<- prop_bl_long(wa_tot, wa_c, wa_ca, wa_p, wa_pa) %>%
   mutate(state = factor("Washington"))
 
 #combine both dataframes
 df <- rbind(df_oregon, df_washington) %>% 
-#df <- df %>% 
+  # df <- df_oregon %>% 
   mutate(period = as.factor(case_when(
     date >= "2018-01-01" & date <= "2018-12-31" ~ "Baseline", 
     date > "2019-04-14" ~ "After", 
     date >= "2019-04-07" & date <= "2019-04-14" ~  "Campaign-week",
-    TRUE ~ "2019 before campaign")))
+    TRUE ~ "2019 before campaign"))) 
 
-write.csv(df, "data/TweetVolumeLongNarrow.csv", row.names=F)
+#write.csv(df, "data/TweetVolumeLongOfEffectExcludingCampaignAuthors.csv", row.names=F)
 
-#rm(list=ls()) #delete variables in environment
+#save file with only the values when authors are excluded
+df1 <- df %>% 
+  filter(keywords=="Campaign wo authors" | keywords=="Prevention wo authors") %>% 
+  droplevels() %>% 
+  mutate(keywords=recode(keywords,"Campaign wo authors"= "Campaign", "Prevention wo authors"="Prevention"))
+
+write.csv(df1, "data/TweetVolumeLong.csv", row.names=F)
+
+rm(list=ls()) #delete variables in environment
 
